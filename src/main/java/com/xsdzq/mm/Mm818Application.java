212be809994025242c3dfb1357fe63f766bcea70
fanjingxi <523777168@qq.com>
2019-07-23 18:52:17 +0800

    submmit

--- src/main/java/com/xsdzq/mm/Mm818Application.java
+++ src/main/java/com/xsdzq/mm/Mm818Application.java
@@ -2,8 +2,10 @@ package com.xsdzq.mm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Mm818Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/xsdzq/mm/ScheduledService.java
@@ -0,0 +1,43 @@
+package com.xsdzq.mm;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.xsdzq.mm.entity.PrizeEntity;
+import com.xsdzq.mm.service.PrizeService;
+
+
+@Component
+@Async
+public class ScheduledService {
+	@Autowired
+	@Qualifier("prizeServiceImpl")
+	PrizeService prizeService;
+	//定时扫描交易任务
+    @Scheduled(cron = "0/5 * * * * *")
+    public void scheduled(){
+      //  log.info("=====>>>>>使用cron  {}",System.currentTimeMillis());
+    	System.out.println("111111111111111111111111111111111111111111111111");
+    	//查看用户交易视图crm，循环交易记录
+    	
+    	List<PrizeEntity> prizeEntities = prizeService.getPrizeAll();
+    	for(PrizeEntity p:prizeEntities) {
+    		System.out.println("奖品：----------------------"+p.getName());
+    	}
+    }
+    @Scheduled(fixedRate = 5000)
+    public void scheduled1() {
+      //  log.info("=====>>>>>使用fixedRate{}", System.currentTimeMillis());
+    	System.out.println("2222222222222222222222222222222222222222222222222222");
+    }
+    @Scheduled(fixedDelay = 5000)
+    public void scheduled2() {
+     //   log.info("=====>>>>>fixedDelay{}",System.currentTimeMillis());
+    	System.out.println("33333333333333333333333333333333333333333333333333333");
+    }
+}
