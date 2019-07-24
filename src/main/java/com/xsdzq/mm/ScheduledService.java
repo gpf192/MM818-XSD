abb6e9f31b23a6c9aef77c736fea26e5742491c8
fanjingxi <523777168@qq.com>
2019-07-24 18:57:28 +0800

    添加定时任务

--- src/main/java/com/xsdzq/mm/ScheduledService.java
+++ src/main/java/com/xsdzq/mm/ScheduledService.java
@@ -35,11 +35,12 @@ public class ScheduledService {
 	@Autowired
 	@Qualifier("userServiceImpl")
 	UserService userService;
-	//定时扫描交易任务
+	
     @Scheduled(cron = "0/5 * * * * *")
     public void scheduled(){
       //  log.info("=====>>>>>使用cron  {}",System.currentTimeMillis());
     	System.out.println("111111111111111111111111111111111111111111111111");
+    	//定时扫描交易任务
     	//查看用户交易视图crm，循环交易记录
     	
     	List<ProductSellViewEntity> productSellViewList = productSellViewService.getByDealTime(20180301);
@@ -62,12 +63,24 @@ public class ScheduledService {
         		}
         		//判断是否有经纪人
         		if(!"0".equals(empId)) {
-        			//判断用户记录表是否存在clientid，
+        			//有经纪人  判断用户记录表是否存在clientid，
+        			//如果不存在  插入用户表  插入用户票数表-票数是0 // 插入用户得票记录表-增票 -自动减票 
+        			//如果存在  插入用户得票记录表-增票 -自动减票 
         		}else {
-        			
+        			//无经纪人 判断用户记录表是否存在clientid，
+        			//如果不存在  插入用户表  插入用户票数表-增票  插入用户得票记录表-增票
+        			//如果存在  更新用户票数表-增票  插入用户得票记录-增票
         		}
         	}
     	}
+    	
+    	//定时扫描用户登录记录 自动归票至经纪人// 用户登录 用户表已经有记录 这里只更新用户票数表-减票   插入用户得票记录表-自动减票
+    	//定时扫描用户分享记录 自动归票至经纪人
+    	//定时扫描用户抽奖获得额外投票权 自动归票至经纪人
+    	
+    	//定时扫描用户活动期间签约投顾 自动归票至经纪人
+    	//定式扫描用户活动期间开通基金账户 自动归票至经纪人
+    	
 
     }
     @Scheduled(fixedRate = 5000)
