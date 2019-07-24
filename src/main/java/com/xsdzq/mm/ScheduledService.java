c6122e8eb4b150a08299f30f9652148a50d545cf
fanjingxi <523777168@qq.com>
2019-07-24 15:04:58 +0800

    7.24-2

--- src/main/java/com/xsdzq/mm/ScheduledService.java
+++ src/main/java/com/xsdzq/mm/ScheduledService.java
@@ -9,7 +9,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.xsdzq.mm.entity.PrizeEntity;
+import com.xsdzq.mm.entity.ProductSellViewEntity;
 import com.xsdzq.mm.service.PrizeService;
+import com.xsdzq.mm.service.ProductSellViewService;
 
 
 @Component
@@ -18,6 +20,10 @@ public class ScheduledService {
 	@Autowired
 	@Qualifier("prizeServiceImpl")
 	PrizeService prizeService;
+	
+	@Autowired
+	@Qualifier("productSellViewServiceImpl")
+	ProductSellViewService productSellViewService;
 	//定时扫描交易任务
     @Scheduled(cron = "0/5 * * * * *")
     public void scheduled(){
@@ -25,10 +31,13 @@ public class ScheduledService {
     	System.out.println("111111111111111111111111111111111111111111111111");
     	//查看用户交易视图crm，循环交易记录
     	
-    	List<PrizeEntity> prizeEntities = prizeService.getPrizeAll();
-    	for(PrizeEntity p:prizeEntities) {
-    		System.out.println("奖品：----------------------"+p.getName());
+    	List<ProductSellViewEntity> productSellViewList = productSellViewService.getByDealTime(20180301);
+    	if(productSellViewList != null) {
+        	for(ProductSellViewEntity p:productSellViewList) {
+        	//	System.out.println("奖品：----------------------"+p.getName());
+        	}
     	}
+
     }
     @Scheduled(fixedRate = 5000)
     public void scheduled1() {
