f2f6a8e45be91b49be4b3df44f4aa13ac5826034
郑金光 <zhengjinguangzjg@163.com>
2020-02-10 14:46:43 +0800

    开门红活动，初始化新表

---
+++ src/main/java/com/xsdzq/mm/controller/AwardController.java
@@ -0,0 +1,5 @@
+package com.xsdzq.mm.controller;
+
+public class AwardController {
+
+}
---
+++ src/main/java/com/xsdzq/mm/dao/AwardResultRepository.java
@@ -0,0 +1,9 @@
+package com.xsdzq.mm.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.xsdzq.mm.entity.AwardResultEntity;
+
+public interface AwardResultRepository extends JpaRepository<AwardResultEntity, Long> {
+
+}
