08c2e93eb60393c2b54c6b9cc1a4441e0f1894ac
郑金光 <zhengjinguangzjg@163.com>
2021-01-20 16:19:42 +0800

    增加JpaAuditing

--- src/main/java/com/xsdzq/mm/Mm818Application.java
+++ src/main/java/com/xsdzq/mm/Mm818Application.java
@@ -2,7 +2,9 @@ package com.xsdzq.mm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Mm818Application {
 
