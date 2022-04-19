719a7f7e9997d393f3a10342c16d710b8585be17
gaopengfei <1921183217@qq.com>
2022-04-19 16:59:00 +0800

    feat 财富狂欢2022_518跑马灯

--- src/main/java/com/xsdzq/mm/dao/impl/PrizeResultRepositoryImpl.java
+++ src/main/java/com/xsdzq/mm/dao/impl/PrizeResultRepositoryImpl.java
@@ -23,7 +23,7 @@ public class PrizeResultRepositoryImpl implements PrizeResultWrapper {
 		//mysql
 		//String sql = "select r.* from `lcj_prize_result` r , `lcj_prize_info` i where r.`prize_id` = i.id and i.`type`=1 order by r.`record_time` desc LIMIT 1";
 	    //oracle
-		String sql ="select * from (select r.* from lcj_prize_result r, lcj_prize_info i where r.prize_id = i.id and i.type = 1 order by r.record_time desc) where rownum = 1";
+		String sql ="select * from (select r.* from lcj_518_2022_prize_result r, lcj_518_2022_prize_info i where r.prize_id = i.id and i.type = 1 order by r.record_time desc) where rownum = 1";
 		PrizeResultEntity prizeResultEntity = null;
 		try {
 			Query query = em.createNativeQuery(sql, PrizeResultEntity.class);
